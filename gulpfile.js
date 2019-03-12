084797e1702449c825063be9d8394227ee2bb470
xinglie.lkf <xinglie.lkf@taobao.com>
2019-03-12 10:18:19 +0800

    add icons-check task

--- gulpfile.js
+++ gulpfile.js
@@ -85,6 +85,55 @@ gulp.task('lang-check', () => {
     console.table(missed);
 });
 
+let htmlIconReg = /(&#x)([0-9a-f]{4})/g;
+let cssIconReg = /(['"])\\([0-9a-f]{4})\1/g;
+let path = require('path');
+let https = require('https');
+gulp.task('icons-check', () => {
+    let exts = {
+        css: 1,
+        less: 1,
+        html: 1,
+        js: 1,
+        ts: 1,
+        mx: 1
+    };
+    let icons = {};
+    combineTool.walk('./tmpl', function (file) {
+        let ext = path.extname(file);
+        if (exts[ext.substring(1)]) {
+            let reg;
+            if (ext == '.css' ||
+                ext == '.less') {
+                reg = cssIconReg;
+            } else {
+                reg = htmlIconReg;
+            }
+            let c = combineTool.readFile(file);
+            c.replace(reg, function (match, ignore, hex) {
+                icons[hex] = 1;
+            });
+        }
+    });
+    https.get('https://www.iconfont.cn/open/project/detail.json?pid=890516', res => {
+        let raw = '',
+            unused = {};
+        res.on('data', d => {
+            raw += d;
+        });
+        res.on('end', () => {
+            let json = JSON.parse(raw);
+            for (let i of json.data.icons) {
+                let n = parseInt(i.unicode).toString(16);
+                if (!icons.hasOwnProperty(n)) {
+                    unused[n] = 'unused';
+                }
+            }
+            console.table(unused);
+        });
+    });
+});
+
 var terser = require('gulp-terser-scoped');
 gulp.task('cleanBuild', () => {
     return del(buildFolder);
